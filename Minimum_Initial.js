e41b52798deb7fd9da94e9a66d96dc09913f971e
Archana123-prog <archana12f@gmail.com>
2026-05-12 18:49:00 +0530

    Correct code

---
+++ Minimum_Initial.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} tasks
+ * @return {number}
+ */
+var minimumEffort = function(tasks) {
+
+    // Sort by (minimum - actual) descending
+    tasks.sort((a, b) => {
+        return (b[1] - b[0]) - (a[1] - a[0]);
+    });
+
+    let ans = 0;
+    let energy = 0;
+
+    for (let [actual, minimum] of tasks) {
+
+        // If current energy is less than minimum required
+        if (energy < minimum) {
+            ans += (minimum - energy);
+            energy = minimum;
+        }
+
+        // Complete the task
+        energy -= actual;
+    }
+
+    return ans;
+};
